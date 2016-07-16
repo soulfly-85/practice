6a32a2ddda92a317db399096a07d03bd1ad800fc
soulfly-85 <soulfly.sem@gmail.com>
2016-07-16 23:09:01 +0700

    end resource user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Practice::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_page#help',    via: 'get'
   match '/about',   to: 'static_page#about',   via: 'get'
   match '/contact', to: 'static_page#contact', via: 'get'
