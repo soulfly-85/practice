42baf70fb750abe34180e663b2693c82a8176b45
soulfly-85 <soulfly.sem@gmail.com>
2016-07-16 18:45:40 +0700

    cofig git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practice::Application.config.secret_key_base = '0ada65ebb394bf97b697512ca137a7e915a4b738e295688f6fa45c0d41a7b7959d3318db2b826f400aa687fff5507aed644928a7b3f01b4885513a2c1b25b9a2'
