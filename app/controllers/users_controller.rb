4cf5ce510b65ea6ec30bfcdb9062ff57e619b12f
soulfly-85 <soulfly.sem@gmail.com>
2016-07-17 17:09:10 +0700

    final Users plus some Microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def index
+	@users = User.paginate(page: params[:page])
+  end
+
   
   def show
     @user = User.find(params[:id])
+	@microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -19,6 +28,25 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    #@user = User.find(params[:id])
+  end
+  
+  def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
   
   private
 
@@ -26,4 +54,21 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+	
+	def signed_in_user
+		unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+	
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+	
+	def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+	
 end
