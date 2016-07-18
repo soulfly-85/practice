0538e8334008f58563e32aa735c829b04f2f1960
soulfly-85 <soulfly.sem@gmail.com>
2016-07-19 00:39:51 +0700

    Some exercises to 5.6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,56 +3,34 @@ require 'spec_helper'
 describe "Static pages" do
 
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
-  describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/home'
-      expect(page).to have_content('Sample App')
-    end
 	
-	it "should have the title 'Home'" do
-      visit '/home'
-      expect(page).to have_title("#{base_title} | Home")
-    end
+	subject { page }
+	
+  describe "Home page" do
+	before { visit root_path }
+    it { should have_content 'Sample App' } 
+ 	
+	it { should have_title 'Home' }
   end
 
   describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit '/help'
-      expect(page).to have_content('Help')
-    end
+	before { visit help_path }
+    it { should have_content 'Help' } 
 	
-	it "should have the title 'Help'" do
-      visit '/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+	it { should have_title 'Help' } 
   end
   
   describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit '/about'
-      expect(page).to have_content('About Us')
-    end
+	before { visit about_path }
+    it { should have_content 'About Us' } 
 	
-	it "should have the title 'About Us'" do
-      visit '/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+	it { should have_title 'About Us' }
   end
   
   describe "Contact page" do
+	before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
 
-    it "should have the content 'Contact'" do
-      visit '/contact'
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit '/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_title 'Contact' }
   end
 end
