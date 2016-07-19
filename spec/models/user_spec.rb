6fb8c3b006d36499fe4b206094e1bed3e4cecb26
soulfly-85 <soulfly.sem@gmail.com>
2016-07-20 00:38:41 +0700

    Rspec of User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe User do
+  before do
+    @user = User.new(name: "soulfly-85", email: "soulfly-85@mail.ru",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+  
+  subject { @user }
+  
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
+  
+  it { should be_valid }
+  
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+	  before { @user.password_confirmation = "mismatch" }
+	  it { should_not be_valid }
+  end
+  
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+  
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,6 +22,7 @@ describe "Static pages" do
   
   describe "About page" do
 	before { visit about_path }
+	
     it { should have_content 'About Us' } 
 	
 	it { should have_title 'About Us' }
